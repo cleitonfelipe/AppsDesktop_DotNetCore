ded7b451f11b86081f453a508b271fa3aeae1e76
Cleiton Felipe de Moraes <cleiton.felipe@yahoo.com.br>
2020-03-13 10:08:04 -0300

    App project WinFormsCli

---
+++ AppWinFormsCli/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinFormsCli
+{
+    public partial class Form1 : Form
+    {
+        private Button btn;
+        public Form1()
+        {
+            InitializeComponent();
+            btn = new Button();
+            btn.Size = new Size(100, 60);
+            btn.Location = new Point(30, 30);
+            btn.Text = "Click Me";
+            this.Controls.Add(btn);
+            btn.Click += new EventHandler(btn_Click);
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Olá .Net Core WinForms");
+        }
+
+    }
+}
