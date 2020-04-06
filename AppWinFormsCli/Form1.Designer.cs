2ca680e6c1a4be32a601144b7f5dce03fe82fb53
Cleiton Felipe de Moraes <cleiton.felipe@yahoo.com.br>
2020-04-06 10:20:53 -0300

    Update Forms com Visual Desiner Preview

--- AppWinFormsCli/Form1.Designer.cs
+++ AppWinFormsCli/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(459, 99);
+            this.button1.Location = new System.Drawing.Point(320, 33);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(146, 101);
             this.button1.TabIndex = 0;
             this.button1.Text = "Click Me 2";
             this.button1.UseVisualStyleBackColor = true;
